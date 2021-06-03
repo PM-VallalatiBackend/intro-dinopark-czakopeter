d4b1ac0e7dfc93579f7fd5f53491fd0e37549148
Czakó Péter <czakopeter@freemail.hu>
2021-06-03 23:24:11 +0200

    Basic

--- src/main/java/hu/progmasters/dinopark/domain/Visitor.java
+++ src/main/java/hu/progmasters/dinopark/domain/Visitor.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class Visitor {
 
-    private static int MIN_REQUIRED_DINO = 3;
+    private static final int MIN_REQUIRED_DINO = 3;
 
     private Integer id;
     private String name;
